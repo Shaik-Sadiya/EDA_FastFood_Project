f3ce80da05e7c99a1c9a3c025be9c603ae1a79fa
Shaik-Sadiya <shaiksadiya2546@gmail.com>
2026-06-28 12:54:57 +0530

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -8,24 +8,24 @@ print("=" * 50)
 
 print(df.describe())
 
-# Calories Analysis
+
 print("\nAverage Calories:", df["Calories"].mean())
 print("Maximum Calories:", df["Calories"].max())
 print("Minimum Calories:", df["Calories"].min())
 
-# Highest Calorie Items
+
 print("\nTop 10 Highest Calorie Items")
 print(
     df.nlargest(10, "Calories")[["Item", "Calories"]]
 )
 
-# Highest Protein Items
+
 print("\nTop 10 Highest Protein Items")
 print(
     df.nlargest(10, "Protein")[["Item", "Protein"]]
 )
 
-# Average Calories by Category
+
 print("\nAverage Calories by Category")
 print(
     df.groupby("Category")["Calories"]
@@ -33,7 +33,7 @@ print(
       .sort_values(ascending=False)
 )
 
-# Average Sodium by Category
+
 print("\nAverage Sodium by Category")
 print(
     df.groupby("Category")["Sodium"]
